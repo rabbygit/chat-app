10895e9c2baee6db4ab2ea4e466bf688f64c4d24
rabbygit <rabbyhossain482@gmail.com>
2020-07-17 04:22:31 +0600

    ready for production

--- src/components/ChatHome.vue
+++ src/components/ChatHome.vue
@@ -1,45 +1,63 @@
 <template>
-  <div class="join-container">
-    <header class="join-header">
-      <h1>
-        <i class="fas fa-smile"></i> My Chat
-      </h1>
-    </header>
-    <div v-if="!logged" class="join-main">
-      <form @submit.prevent="Login">
-        <div class="form-group">
-          <label for="inputState">User Name:</label>
-          <input
-            id="inputState"
-            type="text"
-            class="form-control"
-            v-model="user"
-            placeholder="Enter Your Name"
-            required
-          />
+  <div class="chat">
+    <div class="card border-0">
+      <!-- Chat Header Section -->
+      <header class="card-header rounded-0 px-2">
+        <div class="d-flex">
+          <div>
+            <h5 class="mb-0 font-weight-bold">
+              <i class="fas fa-smile text-info"></i> My Chat
+            </h5>
+          </div>
+          <div class="ml-auto">
+            <h6 class="mb-0">{{welcomeMessage}}</h6>
+          </div>
         </div>
-        <button type="submit" class="btn">Join Chat</button>
-      </form>
-    </div>
-    <div v-else>
-      <h2>{{welcomeMessage}}</h2>
-      <div class="messages">
-        <h4 v-for="(message , index) in newUserJoinAndLeftMessage" :key="index">{{message}}</h4>
-        <p v-for="(data , index) in messages" :key="index">
-          <span>{{data.user}}</span>
-          {{data.message}}
-        </p>
-        <form @submit.prevent="sendMessage">
+      </header>
+      <!-- Card body for user name input -->
+      <div v-if="!logged" class="card-body join-box">
+        <form @submit.prevent="Login">
           <div class="form-group">
+            <label for="inputState">User Name:</label>
             <input
+              id="inputState"
               type="text"
               class="form-control"
-              v-model="message"
-              placeholder="Enter Your Message"
+              v-model="user"
+              placeholder="Enter Your Name"
               required
             />
           </div>
+          <button type="submit" class="btn float-right">Join Chat</button>
         </form>
+      </div>
+      <!-- After entering in chat  -->
+      <div v-else class="card-body message-box my-5">
+        <div class="messages px-2 px-lg-5">
+          <p
+            v-for="(data , index) in messages"
+            :key="index"
+            :class="data.type != 'message' ? 'text-danger' : ''"
+          >
+            <span v-if="data.type == 'message'">{{data.user}}:</span>
+            {{data.message}}
+          </p>
+        </div>
+        <!-- Message Form -->
+        <div class="form-box px-2 px-lg-5 py-2">
+          <form @submit.prevent="sendMessage">
+            <div class="form-group mb-0">
+              <input
+                type="text"
+                v-model="message"
+                class="form-control border-0 shadow-none"
+                placeholder="Enter Your Message"
+                required
+              />
+            </div>
+          </form>
+          <i class="fas fa-paper-plane text-primary"></i>
+        </div>
       </div>
     </div>
   </div>
@@ -52,9 +70,7 @@
     data() {
       return {
         user: "",
-        selectedRoom: "",
         welcomeMessage: "",
-        newUserJoinAndLeftMessage: [],
         logged: false,
         message: "",
         messages: [],
@@ -63,24 +79,37 @@
     },
     methods: {
       Login() {
-        this.socket.emit("SEND_USER", this.user, this.selectedRoom);
+        // Track the user
+        this.socket.emit("SEND_USER", this.user);
 
         this.socket.on("WELCOME", message => {
           this.welcomeMessage = message;
         });
 
+        // Event on new user join
+        this.socket.on("NEW_USER_JOIN", (message, type) => {
+          let newObject = {
+            message,
+            type
+          };
+          this.messages.push(newObject);
+        });
+
+        // Message Option enabled
         this.logged = !this.logged;
 
-        this.socket.on("NEW_USER_JOIN", joinMessage => {
-          this.newUserJoinAndLeftMessage.push(joinMessage);
-        });
-
-        this.socket.on("USER_LEFT", leftMessage => {
-          this.newUserJoinAndLeftMessage.push(leftMessage);
+        // Event on User leaving
+        this.socket.on("USER_LEFT", (message, type) => {
+          let newObject = {
+            message,
+            type
+          };
+          this.messages.push(message, newObject);
         });
       },
+
+      // Event on message send
       sendMessage() {
-        console.log(this.message);
         this.socket.emit("SEND_MESSAGE", {
           user: this.user,
           message: this.message
@@ -89,65 +118,105 @@
       }
     },
     mounted() {
-      this.socket.on("MESSAGE", data => {
-        this.messages = [...this.messages, data];
-        // you can also do this.messages.push(data)
+      // Handle all message
+      this.socket.on("MESSAGE", (data, type) => {
+        let { user, message } = data;
+        let newObject = {
+          user,
+          message,
+          type
+        };
+        this.messages = [...this.messages, newObject];
       });
     }
   };
 </script>
 
 <style scoped>
-  .join-container {
-    max-width: 500px;
-    margin: 80px auto;
-    /* color: #fff; */
-  }
-  .join-header {
-    background: #667aff;
-    text-align: center;
-    padding: 20px;
-    border-top-left-radius: 5px;
-    border-top-right-radius: 5px;
-  }
-  .join-main {
-    background: #7386ff;
-    padding: 30px 40px;
-  }
-  .join-main p {
-    margin-bottom: 20px;
-  }
-  .join-main .form-control {
-    margin-bottom: 20px;
-  }
-  .join-main label {
-    display: block;
-    margin-bottom: 5px;
-  }
-  .join-main input[type="text"] {
-    font-size: 16px;
-    padding: 5px;
-    height: 40px;
+  .chat .card .card-header {
     width: 100%;
+    top: 0;
+    left: 0;
+    position: fixed;
+    background: #ffffff;
+    z-index: 999;
   }
-  .join-main select {
-    font-size: 16px;
-    padding: 5px;
-    height: 40px;
+  .chat .messages {
+    position: fixed;
+    top: 0;
+    left: 0;
     width: 100%;
-  }
-  .join-main .btn {
-    margin-top: 20px;
-    width: 100%;
-  }
-
-  .messages {
-    height: 80%;
+    height: 95vh;
+    padding-top: 60px;
+    padding-bottom: 60px;
     overflow-y: scroll;
   }
-
-  .messages input {
+  .chat .card .form-box {
     position: fixed;
     bottom: 0;
+    left: 0;
+    width: 100%;
+    background: #ffffff;
+    z-index: 999;
+  }
+  .chat .card .form-box .form-control {
+    border-radius: 25px;
+    font-size: 14px;
+    height: 40px;
+    padding-left: 15px;
+    background: rgba(85, 85, 85, 0.144);
+  }
+  .chat .form-control:focus {
+    box-shadow: none;
+  }
+
+  @media (max-width: 576px) {
+    .chat .card .form-box .form-control {
+      padding-right: 45px;
+    }
+    .chat .card .form-box i {
+      position: absolute;
+      top: 18px;
+      right: 25px;
+      font-size: 20px;
+    }
+    .chat .card .join-box {
+      padding-top: 150px;
+      width: 90vw;
+      margin: auto;
+    }
+  }
+
+  @media (min-width: 576px) {
+    .chat .card .form-box .form-control {
+      padding-right: 45px;
+    }
+    .chat .card .form-box i {
+      position: absolute;
+      top: 18px;
+      right: 65px;
+      font-size: 20px;
+    }
+    .chat .card .join-box {
+      padding-top: 150px;
+      width: 450px;
+      margin: auto;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .chat .card .card-header,
+    .chat .card .message-box,
+    .chat .card .message-box .messages,
+    .chat .card .form-box {
+      padding-left: 300px !important;
+      padding-right: 300px !important;
+    }
+    .chat .card .form-box i {
+      position: absolute;
+      top: 18px;
+      right: 320px;
+      font-size: 20px;
+    }
   }
 </style>
